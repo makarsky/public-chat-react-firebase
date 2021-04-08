bc4c8486bd926abaae72f60b8499d2c4feee82fe
Igor Makarsky <makarrski@gmail.com>
2021-04-08 20:43:50 +0000

    Refresh message time every 15 sec

--- src/modules/main/components/MessageList.tsx
+++ src/modules/main/components/MessageList.tsx
@@ -1,12 +1,27 @@
-import React, { FunctionComponent } from 'react';
+import React, { useEffect, useState, FunctionComponent } from 'react';
 import { Data } from 'react-firebase-hooks/firestore/dist/firestore/types';
 import MessageListItem from './MessageListItem';
 import MessageListProps from '../interfaces/MessageListProps';
+
+const timeoutInSeconds = 15;
+
+let interval: NodeJS.Timeout;
 
 const MessageList: FunctionComponent<MessageListProps> = ({
   user,
   messages,
 }: MessageListProps) => {
+  const [, setHash] = useState(0);
+
+  useEffect(() => {
+    interval = setInterval(() => {
+      setHash((v) => v + 1);
+    }, timeoutInSeconds * 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
     <>
       {messages &&
