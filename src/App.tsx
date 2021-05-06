cdcd73dc0048d2292c563b88eef72e5b7a8b3641
Igor Makarsky <makarrski@gmail.com>
2021-05-06 21:38:17 +0000

    Implement custom theme with dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
-import React, { FunctionComponent } from 'react';
+import React, { useState, useEffect, FunctionComponent } from 'react';
 import './App.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import firebaseProvider from './firebase';
 import DefaultLayout from './modules/main/layouts/DefaultLayout';
 import Loader from './modules/main/layouts/Loader';
 
 const App: FunctionComponent = () => {
   const [user] = useAuthState(firebaseProvider.auth);
+  const [isDarkMode, setIsDarkMode] = useState(
+    useMediaQuery('(prefers-color-scheme: dark)'),
+  );
 
-  return user ? <DefaultLayout user={user} /> : <Loader />;
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  useEffect(() => setIsDarkMode(prefersDarkMode), [prefersDarkMode]);
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: isDarkMode ? 'dark' : 'light',
+          primary: {
+            light: isDarkMode ? '#616161' : '#2c387e',
+            main: isDarkMode ? '#424242' : '#3f51b5',
+            dark: isDarkMode ? '#1976d2' : '#fffde7',
+          },
+          secondary: {
+            main: isDarkMode ? '#ffee58' : '#5c6bc0',
+          },
+          background: {
+            default: isDarkMode ? '#212121' : '#e8eaf6',
+          },
+        },
+      }),
+    [isDarkMode],
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      {user ? <DefaultLayout user={user} /> : <Loader />}
+    </ThemeProvider>
+  );
 };
 
 export default App;
