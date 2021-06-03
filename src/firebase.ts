9599906c0914d3883e3d19aae9b27edd12971a2a
Igor Makarsky <makarrski@gmail.com>
2021-06-03 22:02:14 +0000

    Remove comments

--- src/firebase.ts
+++ src/firebase.ts
@@ -26,16 +26,6 @@ class FirebaseProvider {
     return this.auth.signOut();
   }
 
-  // addQuote(quote) {
-  //   if(!this.auth.currentUser) {
-  //     return alert('Not authorized')
-  //   }
-
-  //   return this.firestore.doc(`users_codedamn_video/${this.auth.currentUser.uid}`).set({
-  //     quote
-  //   })
-  // }
-
   isInitialized() {
     return new Promise((resolve) => {
       this.auth.onAuthStateChanged(resolve);
@@ -45,30 +35,6 @@ class FirebaseProvider {
   getUserUid() {
     return this.auth.currentUser && this.auth.currentUser.uid;
   }
-
-  // async getUserLastMessageDate() {
-  //   if (this.auth.currentUser) {
-  //     const data = await this.firestore
-  //       .doc(`users/${this.auth.currentUser.uid}`)
-  //       .get();
-  //     const rateLimitData = data.get('rateLimit');
-
-  //     return rateLimitData?.lastMessage;
-  //   }
-
-  //   return null;
-  // }
-
-  // async getMessages() {
-  //   if (this.auth) {
-  //     const quote = await this.firestore
-  //       .doc(`messages/${this.auth.currentUser.uid}`)
-  //       .get();
-
-  //     return quote.get('quote');
-  //   }
-  //   return [];
-  // }
 }
 
 export default new FirebaseProvider();
