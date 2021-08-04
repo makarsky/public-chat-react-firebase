8476074fd6d88fc346062f0fa22ece207594e315
Igor Makarsky <makarrski@gmail.com>
2021-08-04 21:02:58 +0000

    Revert "Remove firebase analytics initialization"
    This reverts commit d5d18c18af0d278c1a0b59340daf0691df197e5e.

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,6 +3,8 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 // eslint-disable-next-line import/no-duplicates
 import 'firebase/firestore';
+// eslint-disable-next-line import/no-duplicates
+import 'firebase/analytics';
 import config from './config/firebase';
 
 class FirebaseProvider {
@@ -10,10 +12,13 @@ class FirebaseProvider {
 
   firestore: firebase.firestore.Firestore;
 
+  analytics: firebase.analytics.Analytics;
+
   constructor() {
     firebase.initializeApp(config);
     this.auth = firebase.auth();
     this.firestore = firebase.firestore();
+    this.analytics = firebase.analytics();
     this.auth.signInAnonymously();
   }
 
