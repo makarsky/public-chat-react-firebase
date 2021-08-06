dafa690dafc716e3e35b3f82e98cefa9d810fd3e
Igor Makarsky <makarrski@gmail.com>
2021-08-06 13:35:52 +0000

    Update hardCodedMessages

--- src/modules/main/configs/hardCodedMessages.ts
+++ src/modules/main/configs/hardCodedMessages.ts
@@ -9,7 +9,7 @@ export default [
         avatarSrc: 'https://avatars.githubusercontent.com/u/22708426?v=4',
       },
       value:
-        'Welcome to the Emoji chat!\nSince people love to post nasty things and there is no decent bad word filter library, only emojis and gifs are allowed :upside_down_face:\nFeel free to click on everything you see :eyes:',
+        'Welcome to the Emoji chat!\nSince people love to post nasty things and there is no decent bad word filter library, only emojis and gifs are allowed. User names are randomly generated :upside_down_face:\nFeel free to click on everything you see :eyes:',
       timestamp: {
         seconds: 1628245900,
       },
