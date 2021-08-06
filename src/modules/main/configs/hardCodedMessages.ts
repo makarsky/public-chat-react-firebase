ec786a0275617f02c16634cda784e9a40d5be305
Igor Makarsky <makarrski@gmail.com>
2021-08-06 09:45:13 +0000

    Update the hardcoded message

--- src/modules/main/configs/hardCodedMessages.ts
+++ src/modules/main/configs/hardCodedMessages.ts
@@ -8,7 +8,8 @@ export default [
         colorIndex: 1,
         avatarSrc: 'https://avatars.githubusercontent.com/u/22708426?v=4',
       },
-      value: 'Welcome to the anonymous chat! Feel free to use emojis and gifs!',
+      value:
+        'Welcome to the Emoji chat!\nSince people love to post nasty things and there is no decent bad word filter library, only emojis and gifs are allowed :upside_down_face:\nFeel free to click on everything you see :eyes:',
       timestamp: {
         seconds: 1625340321,
       },
